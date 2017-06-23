3ae064b8c4eead796a83944361463acf10078dd9
Benjamin Landers <benjaminrlanders@gmail.com>
2017-06-22 20:35:42 -0700

    add old improvement to the parser fixes #7

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 var input = document.getElementById("input");
 var out = document.getElementById("output");
 
-input.onkeydown = e=>{
-  
-  if(e.keyCode === 13){
-    var exp = pl(input.value);
-    var wrong = findWrong(exp);
-    if(!wrong){
-      out.textContent = prettify(...writeProof(prove(exp)));
-    }else{
-      out.textContent = 'Not a tautology, the variable binding:\n'+ [...wrong.keys()].map(key=>''+key+': '+wrong.get(key)).join('\n')+'\n disproves it.';
-    }
+input.onkeydown = ()=>{
+  setTimeout(process,0);
+};
+
+var process = ()=>{
+  var exp = flexibleParse(lex(input.value));
+  if(str(exp) !== input.value){
+    out.textContent = input.value + ' is not a formally valid formula, interpretting as ' + str(exp) + '\n';
+  }else{
+    out.textContent = '';
+  }
+  var wrong = findWrong(exp);
+  if(!wrong){
+    out.textContent += prettify(...writeProof(pruneSteps(prove(exp))));
+  }else{
+    out.textContent += 'Not a tautology, the variable binding:\n'+ [...wrong.keys()].map(key=>''+key+': '+wrong.get(key)).join('\n')+'\n disproves it.';
   }
 };
